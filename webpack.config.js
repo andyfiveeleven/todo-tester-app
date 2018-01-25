4edd0d5426a37f9c1201494d78a8a61216991f30
andyfiveeleven <fouracesguitar@gmail.com>
2018-01-25 12:55:24 -0800

    setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
 'use strict';
 
-//webpack's main purpose 
+//webpack's main purpose is to handle all the extra script tags and concats it all into single file. called bundle. also does that with SCSS into the head
 
-const HtmlPlugin = require('html-webpack-plugin');
-const ExtractPlugin = require('extract-text-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');//with html
+const ExtractPlugin = require('extract-text-webpack-plugin');//with css
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
-  entry: `${__dirname}/src/main.js`,
-  output: {
+  devServer: { //gives us the ability to hook into stuff in dev server
+    historyApiFallback: true //history shit
+  },
+  entry: `${__dirname}/src/main.js`,//where webpack reads app
+  output: { //drop our final build into this directory
     filename: 'bundle-[hash].js',
     path: `${__dirname}/build`,
     publicPath: '/'
@@ -17,12 +20,12 @@ module.exports = {
     new HtmlPlugin({ template: `${__dirname}/src/index.html`}),
     new ExtractPlugin('bundle-[hash].css')
   ],
-  module: {
+  module: { //holds all the loaders as part of our app
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
+        test: /\.js$/, //look for this (regex)
+        exclude: /node_modules/, //cept this
+        loader: 'babel-loader' //do this
       },
       {
         test: /\.scss$/,
